fa35eace0d05c3b6e0af44780dab6584e1fd69f6
danlooo <loosdaniel@gmail.com>
2021-05-01 18:24:24 +0200

    Update deploy.sh: Add docker push

--- deploy.sh
+++ deploy.sh
@@ -4,4 +4,8 @@ docker pull danlooo/icarus-frontend || :
 docker pull danlooo/icarus-backend || :
 
 docker-compose build
+
+docker push danlooo/icarus-frontend || :
+docker push danlooo/icarus-backend || :
+
 docker-compose up
